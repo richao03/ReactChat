b4e56b243e59ab8203cdb6254f0d1b78ea8e7d53
Richard Chao <r.chao03@gmail.com>
2016-12-28 16:03:27 -0500

    name reset

--- routes/socket.js
+++ routes/socket.js
@@ -1,39 +1,48 @@
+
 // Keep track of which names are used so that there are no duplicates
 var userNames = (function () {
   var names = {};
 
-  var check = function (name) {
+  var claim = function (name) {
     if (!name || names[name]) {
-      console.log(name,"name in use")
       return false;
     } else {
-      console.log(name, "name not in use")
       names[name] = true;
       return true;
     }
   };
 
-// find the lowest unused "MC" number and check it
+  // find the lowest unused "guest" name and claim it
   var getGuestName = function () {
     var name,
       nextUserId = 1;
+
     do {
-      name = 'MC ' + nextUserId;
+      name = 'Battler ' + nextUserId;
       nextUserId += 1;
-    } while (!check(name));
+    } while (!claim(name));
+
     return name;
   };
 
-//put name in array
+  // serialize claimed names as an array
   var get = function () {
-    var answer = [];
+    var res = [];
     for (user in names) {
-      answer.push(user);
+      res.push(user);
     }
-    return answer;
+
+    return res;
   };
 
 
+
+  return {
+    claim: claim,
+    get: get,
+    getGuestName: getGuestName
+  };
+}());
 
 // export function for listening to the socket
 module.exports = function (socket) {
@@ -58,10 +67,30 @@ module.exports = function (socket) {
     });
   });
 
+  // validate a user's name change, and broadcast it on success
+  socket.on('change:name', function (data, fn) {
+    if (userNames.claim(data.name)) {
+      var oldName = name;
+      userNames.free(oldName);
+
+      name = data.name;
+      
+      socket.broadcast.emit('change:name', {
+        oldName: oldName,
+        newName: name
+      });
+
+      fn(true);
+    } else {
+      fn(false);
+    }
+  });
+
   // clean up when a user leaves, and broadcast it to other users
   socket.on('disconnect', function () {
     socket.broadcast.emit('user:left', {
       name: name
     });
+    userNames.free(name);
   });
 };
